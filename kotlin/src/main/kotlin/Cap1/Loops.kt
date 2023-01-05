013d443175ee58526e154efe47891b6c019ebfd7
renanmuniz <renan.merlo@paygo.com.br>
2023-01-04 22:37:33 -0300

    Cap 1 - Loops

---
+++ kotlin/src/main/kotlin/Cap1/Loops.kt
@@ -0,0 +1,39 @@
+package Cap1
+
+fun main() {
+    println("While:")
+    var x = 0
+    while(x<10) {
+        println("x is: $x")
+        x++
+    }
+
+    println("For:")
+    val list = listOf(1,2,3,4,5,6,7,8,9)
+    for(n in list) {
+        println(n)
+    }
+
+    println("For using range:")
+    for(n in 1..5) {
+        println(n)
+    }
+
+    println("For using range:")
+    val oneToTen = 1..10
+    for(n in oneToTen) {
+        println(n)
+    }
+
+    println("For in Strings")
+    val spellMyName = "Renan Muniz Merlo"
+    for(char in spellMyName) {
+        println(char)
+    }
+
+    println("Index:")
+    val arrayOfNumbers = arrayOf(1,2,3,4,5,6,7,8,9,0)
+    for(index in arrayOfNumbers.indices) {
+        println("Elemento $index é ${arrayOfNumbers[index]}")
+    }
+}
